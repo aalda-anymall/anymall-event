25c8f82e9884ac907ffda048649654fafd83af66
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2026-03-19 20:14:02 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,8 @@ const notoSerifJP = Noto_Serif_JP({
 });
 
 export const metadata: Metadata = {
-  title: "AnyMall ランチイベント",
+  title:
+    "AnyMall 獣医師監修 愛犬・愛猫のためのウェルネスランチイベント申し込み受付中！",
   description:
     "愛犬との暮らしがもっと豊かになる、体験型イベント。食事・健康・ライフスタイルを、専門家と一緒に楽しく学びませんか。",
 };
